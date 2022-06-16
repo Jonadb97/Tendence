d629183a8b286a65115eaacdf5504c28d70d6e9b
matias rios <rios.matias998@gmail.com>
2022-06-16 17:45:06 -0300

    correccion de nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
-        { src: '~/plugins/axios', ssr: false }
+        { src: '~/plugins/axios', ssr: false },
     ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,6 +38,7 @@ export default {
         // https://go.nuxtjs.dev/tailwindcss
         '@nuxtjs/tailwindcss',
         '@nuxtjs/fontawesome',
+
     ],
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
